2a8be37cc5d83b49411d9bcbbd220499205eafc2
songkyung <min.songkyung@gmail.com>
2024-01-16 21:29:33 +0900

    BOJ2178 메모리 초과 오류 해결

--- JavaPractice/src/BFS/BOJ2178_bfs.java
+++ JavaPractice/src/BFS/BOJ2178_bfs.java
@@ -13,11 +13,9 @@ import java.util.StringTokenizer;
 public class BOJ2178_bfs {
     static int[][] arr;
     static boolean[][] visited;
-    public static void bfs(int row, int col) {
-        int n = arr.length;;
+    public static int bfs(int row, int col) {
+        int n = arr.length;
         int m = arr[0].length;
-
-        System.out.println(Arrays.toString(arr[0]));
 
         // 시작점 방문표시
         visited[row][col] = true;
@@ -28,7 +26,7 @@ public class BOJ2178_bfs {
         // Queue 선언
         Queue<int []> queue = new ArrayDeque<>();
         // 시작 지점 (x, y) 큐에 삽입
-        queue.offer(new int[] {row, col});
+        queue.offer(new int[] {row, col, arr[row][col]});
 
         // 큐 빌때까지 실행
         while (!queue.isEmpty()) {
@@ -38,6 +36,12 @@ public class BOJ2178_bfs {
             int x = current[0];
             // col
             int y = current[1];
+            int count = current[2];
+
+            if (x == n-1 && y == m-1) {
+                // 최단 경로 반환
+                return count;
+            }
 
 //            System.out.println("queuecheck:" + x + " " + y);
 
@@ -57,20 +61,23 @@ public class BOJ2178_bfs {
 //                        System.out.println("dxdy:" + dx + " " + dy);
 //                        System.out.println("dirscheck:" + arr[dx][dy] + " " + !visited[dx][dy]);
 
-                        visited[x][y] = true;
+                        visited[dx][dy] = true;
                         // depth 값을 갱신해줌
                         arr[dx][dy] = arr[current[0]][current[1]] + 1;
+                        count = arr[dx][dy];
 
 //                        System.out.println("depth:" + current[0] + " " + current[1]);
                         System.out.println("depth check:" + arr[dx][dy]);
 
                         // 새로운 x,y를 만들어 큐에 삽입
-                        queue.add(new int[] {dx, dy});
+                        queue.add(new int[] {dx, dy, count});
 
                     }
                 }
             }
         }
+        // 도착하지 못한 경우
+        return  0;
     }
 
     public static void main(String[] args) throws IOException {
@@ -90,9 +97,11 @@ public class BOJ2178_bfs {
             }
         }
 
-        bfs(0, 0);
-        // 인덱스가 0부터 시작하니 마지막 칸의 좌표값에 -1을 해줘야함.
-        System.out.println(arr[n-1][m-1]);
+//        bfs(0, 0);
+//        // 인덱스가 0부터 시작하니 마지막 칸의 좌표값에 -1을 해줘야함.
+//        System.out.println(arr[n-1][m-1]);
+
+        System.out.println(bfs(0, 0));
 
         for (int a[]: arr) {
             System.out.println(Arrays.toString(a));
