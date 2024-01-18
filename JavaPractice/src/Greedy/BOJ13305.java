7bc4196ea099028080f80688e8f84980f3f3d174
songkyung <min.songkyung@gmail.com>
2024-01-18 17:28:47 +0900

    BOJ13305 주유소 int->long 수정

--- JavaPractice/src/Greedy/BOJ13305.java
+++ JavaPractice/src/Greedy/BOJ13305.java
@@ -2,35 +2,33 @@ package Greedy;
 
 import java.util.Scanner;
 
-import static java.lang.Math.min;
-
 // 주유소
 public class BOJ13305 {
 
     // 주유비가 가장 싼 곳에서 많이 넣어야함.
 
-    private static int minimum_cost(int[] distances, int[] costs) {
-        int total_min_cost = 0;
-        // 최소 비용 초기화
-        int min_cost = costs[0];
-
-        for (int i=0; i<distances.length; i++) {
-            // 현재까지의 최소 주유비 갱신
-            min_cost = min(min_cost, costs[i]);
-
-            System.out.println("min:" + min_cost + " " + costs[i]  + " " + distances[i]);
-            // 최소 주유비로 이동하는 비용 계산
-            total_min_cost += min_cost * distances[i];
-        }
-
-        return  total_min_cost;
-    }
+//    private static int minimum_cost(int[] distances, int[] costs) {
+//        int total_min_cost = 0;
+//        // 최소 비용 초기화
+//        int min_cost = costs[0];
+//
+//        for (int i=0; i<distances.length; i++) {
+//            // 현재까지의 최소 주유비 갱신 (현재 주유소가 이전 주유소보다 쌀 경우 최소비용 갱신)
+//            min_cost = min(min_cost, costs[i]);
+//
+//            System.out.println("min:" + min_cost + " " + costs[i]  + " " + distances[i]);
+//            // 최소 주유비로 이동하는 비용 계산
+//            total_min_cost += (min_cost * distances[i]);
+//        }
+//
+//        return  total_min_cost;
+//    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] distances = new int[n-1];
-        int[] costs = new int[n];
+        long[] distances = new long[n-1];
+        long[] costs = new long[n];
 
         for (int i=0; i<n-1; i++) {
             distances[i] = sc.nextInt();
@@ -40,7 +38,22 @@ public class BOJ13305 {
             costs[i] = sc.nextInt();
         }
 
-        int result = minimum_cost(distances, costs);
-        System.out.println(result);
+        long total_min_cost = 0;
+        // 최소 비용 초기화
+        long min_cost = costs[0];
+
+
+        for (int i=0; i<n-1; i++) {
+            // 현재까지의 최소 주유비 갱신 (현재 주유소가 이전 주유소보다 쌀 경우 최소비용 갱신)
+
+            if (min_cost > costs[i])
+                min_cost = costs[i];
+
+            System.out.println("min:" + min_cost + " " + costs[i]  + " " + distances[i]);
+            // 최소 주유비로 이동하는 비용 계산
+            total_min_cost += (min_cost * distances[i]);
+        }
+
+        System.out.println(total_min_cost);
     }
 }
