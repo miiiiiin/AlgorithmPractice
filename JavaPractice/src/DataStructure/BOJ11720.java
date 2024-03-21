59608ccfb3d7f479e8fc990be1f549c4c72173bc
songkyung <min.songkyung@gmail.com>
2024-03-21 12:03:01 +0900

    BOJ11720

--- JavaPractice/src/DataStructure/BOJ11720.java
+++ JavaPractice/src/DataStructure/BOJ11720.java
@@ -1,19 +1,22 @@
 package DataStructure;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BOJ11720 {
-
-    static ArrayList<Integer> arr;
     public static void main(String[] args) {
+        int sum = 0;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        arr = new ArrayList<>(n+1);
 
-        for (int i=0; i<=n; i++) {
-            arr.add(sc.nextInt());
+        String number = sc.next();
+        char[] numArr = number.toCharArray();
+
+        for (int i=0; i<numArr.length; i++) {
+            // 아스키코드를 이용해서 cNum의 문자형의 값을 int로 바꿀 때는 문자열 '0'의 값을 빼줌(-48)
+            sum += numArr[i] - '0';
+//            sum += Integer.parseInt(String.valueOf(numArr[i]));
         }
 
+        System.out.println(sum);
     }
 }
