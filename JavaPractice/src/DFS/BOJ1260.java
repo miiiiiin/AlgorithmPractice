a8c5baa10050f92bc72553baea90779c61b4dc66
songkyung <min.songkyung@gmail.com>
2024-04-05 00:04:49 +0900

    BOJ 1260

--- JavaPractice/src/DFS/BOJ1260.java
+++ JavaPractice/src/DFS/BOJ1260.java
@@ -26,8 +26,8 @@ public class BOJ1260 {
         dfsOrder = "";
         bfsOrder = "";
 
-        for (int i=1; i<m; i++) {
-            // 인접리스트 초기화
+        for (int i=1; i<=n; i++) {
+            // 인접 리스트 초기화
             arr[i] = new ArrayList<Integer>();
         }
 
@@ -37,9 +37,12 @@ public class BOJ1260 {
             int y = Integer.parseInt(st.nextToken());
             arr[x].add(y);
             arr[y].add(x);
+
+            // 오름차순 정렬
+            Collections.sort(arr[x]);
+            Collections.sort(arr[y]);
         }
 
-//        System.out.println(Arrays.toString(arr));
         DFS(v);
         BFS(v);
         System.out.println(dfsOrder);
@@ -48,7 +51,6 @@ public class BOJ1260 {
 
     public static void DFS(int index) {
         dfsOrder += index + " ";
-        System.out.println(index);
         if (dfsVisited[index]) {
             return;
         }
@@ -60,22 +62,20 @@ public class BOJ1260 {
         }
     }
 
-    public static void BFS(int index) {
+    public static void BFS(int node) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(index);
-
+        queue.add(node);
+        bfsVisited[node] = true;
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-//            System.out.println("curr check:" + curr + " " + arr[curr]);
-            bfsVisited[curr] = true;
             bfsOrder += curr + " ";
 
             for (int i: arr[curr]) {
-//                System.out.println("check i:" + i + " " + Arrays.toString(bfsVisited) + " " + bfsVisited[i] );
                 // 방문 조건 : 현재 노드와 다음 노드가 연결된 상태이고 아직 방문하지 않은 상태일 때
-                // 이미 방문하지 않은 경우 계속
-                if (bfsVisited[i]) break;
-                queue.add(i);
+                if (!bfsVisited[i]) {
+                    bfsVisited[i] = true;
+                    queue.add(i);
+                }
             }
         }
     }
