b2061d8842c019138f92866a79ec684a876b2c64
songkyung <min.songkyung@gmail.com>
2024-04-04 19:58:45 +0900

    BOJ1260 bfs order

--- JavaPractice/src/DFS/BOJ1260.java
+++ JavaPractice/src/DFS/BOJ1260.java
@@ -3,13 +3,13 @@ package DFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
 public class BOJ1260 {
     static int n, m, v;
     static ArrayList<Integer>[] arr;
-    static boolean[] visited;
+    static boolean[] dfsVisited;
+    static boolean[] bfsVisited;
     static String dfsOrder;
     static String bfsOrder;
     public static void main(String[] args) throws IOException {
@@ -21,7 +21,8 @@ public class BOJ1260 {
         v = Integer.parseInt(st.nextToken());
 
         arr = new ArrayList[n+1];
-        visited = new boolean[n+1];
+        dfsVisited = new boolean[n+1];
+        bfsVisited = new boolean[n+1];
         dfsOrder = "";
         bfsOrder = "";
 
@@ -38,26 +39,44 @@ public class BOJ1260 {
             arr[y].add(x);
         }
 
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
         DFS(v);
+        BFS(v);
         System.out.println(dfsOrder);
+        System.out.println(bfsOrder);
     }
 
     public static void DFS(int index) {
         dfsOrder += index + " ";
         System.out.println(index);
-        if (visited[index]) {
+        if (dfsVisited[index]) {
             return;
         }
-        visited[index] = true;
+        dfsVisited[index] = true;
         for (int i: arr[index]) {
-            if (!visited[i]) {
+            if (!dfsVisited[i]) {
                 DFS(i);
             }
         }
     }
 
     public static void BFS(int index) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(index);
 
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+//            System.out.println("curr check:" + curr + " " + arr[curr]);
+            bfsVisited[curr] = true;
+            bfsOrder += curr + " ";
+
+            for (int i: arr[curr]) {
+//                System.out.println("check i:" + i + " " + Arrays.toString(bfsVisited) + " " + bfsVisited[i] );
+                // 방문 조건 : 현재 노드와 다음 노드가 연결된 상태이고 아직 방문하지 않은 상태일 때
+                // 이미 방문하지 않은 경우 계속
+                if (bfsVisited[i]) break;
+                queue.add(i);
+            }
+        }
     }
 }
