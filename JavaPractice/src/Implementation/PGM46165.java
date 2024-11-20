28aa87f5747c3c6d0c8ce400c263a89812aa80a4
songkyung <min.songkyung@gmail.com>
2024-11-20 10:28:43 +0900

    SWE2072

---
+++ JavaPractice/src/Implementation/PGM46165.java
@@ -0,0 +1,2 @@
+package Implementation;public class PGM46165 {
+}
--- JavaPractice/src/SWE/SWE2072.java
+++ JavaPractice/src/SWE/SWE2072.java
@@ -1,2 +1,26 @@
-package SWE;public class SWE2072 {
+package SWE;
+
+import java.util.Scanner;
+
+public class SWE2072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int sum = 0;
+            for (int j=0; j<10; j++) {
+                int num = sc.nextInt();
+                if (num % 2 != 0) {
+                    sum += num;
+                }
+            }
+            System.out.println("#" + test_case + " " + sum);
+        }
+    }
 }
