f38e5d81e2117f03599286c9590315a73d2b2316
songkyung <min.songkyung@gmail.com>
2024-11-21 17:39:54 +0900

    BOJ1541

--- JavaPractice/src/Greedy/BOJ1541.java
+++ JavaPractice/src/Greedy/BOJ1541.java
@@ -2,38 +2,37 @@ package Greedy;
 
 import java.util.Scanner;
 
-// 잃어버린 괄호
 public class BOJ1541 {
 
-    static int result;
+    static int result = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        // - 기준으로 나눔
+        String[] splitted = str.split("-");
+
+        for (int i=0; i<splitted.length; i++) {
+            int sumValue = sum(splitted[i]);
+
+            // -로 나뉘어진 부분
+            if (i == 0) {
+                result += sumValue;
+            } else {
+                // + 부분
+                result -= sumValue;
+            }
+        }
+        System.out.println(result);
+    }
 
     private static int sum(String line) {
         int sum = 0;
-
+        // + 기준으로 나눔
         String[] cal = line.split("[+]");
         for (int i=0; i<cal.length; i++) {
             sum += Integer.parseInt(cal[i]);
         }
         return sum;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        String line = sc.nextLine();
-        String[] cal = line.split("-");
-
-        for (int i = 0; i < cal.length; i++) {
-            int sum_result = sum(cal[i]);
-
-            // 가장 앞의 데이터일 때
-            if (i == 0) {
-                result += sum_result;
-            } else {
-                result -= sum_result;
-            }
-        }
-        System.out.println(result);
     }
 }
---
+++ JavaPractice/src/Greedy/BOJ1931.java
@@ -0,0 +1,2 @@
+package Greedy;public class BOJ1931 {
+}
