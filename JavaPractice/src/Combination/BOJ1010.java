77ae3dd8906e6dc5e8dbce831a38261f543f4ba8
songkyung <min.songkyung@gmail.com>
2024-12-07 13:54:14 +0900

    BOJ1010

--- JavaPractice/src/Combination/BOJ1010.java
+++ JavaPractice/src/Combination/BOJ1010.java
@@ -1,2 +1,33 @@
-package Combination;public class BOJ1010 {
+package Combination;
+
+import java.util.Scanner;
+
+public class BOJ1010 {
+    static int[][] D;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        D = new int[31][31];
+
+        for (int i=0; i<=30; i++) {
+            // 테이블 초기화
+            D[i][0] = 1;
+            D[i][i] = 1;
+            D[i][1] = i;
+        }
+
+        for (int i=2; i<=30; i++) {
+            for (int j=1; j<=i; j++) {
+                D[i][j] = D[i-1][j-1] + D[i-1][j];
+            }
+        }
+
+        for (int t=0; t<T; t++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            System.out.println(D[M][N]);
+        }
+    }
 }
