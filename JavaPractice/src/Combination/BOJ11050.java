e8e491dcad3b4c6a420c24ea8f63b5d1f0308183
songkyung <min.songkyung@gmail.com>
2024-12-08 23:06:26 +0900

    BOJ11050

--- JavaPractice/src/Combination/BOJ11050.java
+++ JavaPractice/src/Combination/BOJ11050.java
@@ -6,7 +6,7 @@ public class BOJ11050 {
 
     static int n;
     static int k;
-    static int D[][];
+    static int[][] D;
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -16,8 +16,11 @@ public class BOJ11050 {
 
         for (int i=0; i<=n; i++) {
             // 테이블 초기화
+            // i개 중 i개 모두 선택하는 경우의 수는 1개
             D[i][i] = 1;
+            // i개 중 아무 것도 뽑지 않는 경우의 수는 1개
             D[i][0] = 1;
+            // i개 중 1개 뽑는 경우의 수는 i개
             D[i][1] = i;
         }
 
